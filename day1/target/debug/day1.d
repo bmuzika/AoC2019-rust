ce7809ddf81bd3c234a4d8628f434f561acd3c4e
bmuzika <bmuzika@users.noreply.github.com>
2019-12-10 00:31:47 -0500

    Initial commit with intended structure.

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/home/bmuzika/Documents/projects/rust-projects/AoC2019-rust/day1/target/debug/day1: /home/bmuzika/Documents/projects/rust-projects/AoC2019-rust/day1/src/main.rs
---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/home/bmuzika/Documents/projects/rust-projects/AoC2019-rust/day2/target/debug/day2: /home/bmuzika/Documents/projects/rust-projects/AoC2019-rust/day2/src/main.rs
