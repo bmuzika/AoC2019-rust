e09539855f48eee1f708ca6040df8cd991b68b09
Bill Muzika <bill.muzika@outlook.com>
2019-12-11 02:30:52 -0500

    Completed parts 1 and 2. The Intcode processor should be refactored to enable better reuse.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,38 @@
-use std::ops::Deref;
+use std::fs::{File, read_to_string};
+use std::io::Read;
+use std::char::from_digit;
 
 fn main() {
-    println!("Hello, world! It's day2!!!!");
+    let mut file = match File::open("input.txt") {
+        Ok(file) => file,
+        Err(_) => panic!("no file"),
+    };
+
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents)
+        .ok()
+        .expect("failed to read");
+
+    let mut noun = 0;
+    let mut verb = 0;
+
+    'outer: for noun in (0 .. 100) {
+        for verb in (0..100) {
+            let mut working_input_chars = file_contents.clone().trim().split(',').map(|s| s.to_string()).collect::<Vec<String>>();
+            working_input_chars[1] = noun.to_string();
+            working_input_chars[2] = verb.to_string();
+
+            let working_input = working_input_chars.into_iter().collect::<Vec<String>>().join(",");
+
+            let output: String = process_input(working_input.as_str());
+
+            let result = output.trim().split(',').map(|s| s.to_string()).next().unwrap().parse::<u32>().unwrap();
+            if result == 19690720 {
+                println!("noun: {}, verb: {}",noun, verb );
+                break 'outer;
+            }
+        }
+    }
 }
 
 enum IntcodeParseError {
@@ -11,7 +42,8 @@ enum IntcodeParseError {
 
 fn process_input(input: &str) -> String {
     // creates the vector from the program code
-    let input_vector = input.split(',')
+    let input_vector = input.trim()
+        .split(',')
         .map(|s| s.to_string())
         .filter(|s| s != "")
         .map(|s| s.parse::<u64>().unwrap())
@@ -19,21 +51,21 @@ fn process_input(input: &str) -> String {
 
     let mut cursor = 0;
     let mut code_to_execute = input_vector.clone();
-    let slice_to_work_on = code_to_execute[cursor..cursor+4];
+    while (cursor < code_to_execute.len() && code_to_execute[cursor] != 99) {
+        let mut slice_to_work_on = [0,0,0,0];
+        slice_to_work_on.clone_from_slice(&code_to_execute[cursor..cursor + 4]);
 
-    let mut position = code_to_execute.iter();
-
-    while let Some(opcode) = position.next() {
+        let opcode = slice_to_work_on[0];
         let calc: Result<u64, IntcodeParseError> = match opcode {
             1 => {
-                let reg1: usize = *(position.next().expect("Invalid index")) as usize;
-                let reg2: usize = *(position.next().expect("Invalid index")) as usize;
-                Ok(code_to_execute[reg1]+code_to_execute[reg2])
+                let reg1: usize = slice_to_work_on[1] as usize;
+                let reg2: usize = slice_to_work_on[2] as usize;
+                Ok(code_to_execute[reg1] + code_to_execute[reg2])
             }, // Add opcode
             2 => {
-                let reg1: usize = *(position.next().expect("Invalid index")) as usize;
-                let reg2: usize = *(position.next().expect("Invalid index")) as usize;
-                Ok(code_to_execute[reg1]*code_to_execute[reg2])
+                let reg1: usize = slice_to_work_on[1] as usize;
+                let reg2: usize = slice_to_work_on[2] as usize;
+                Ok(code_to_execute[reg1] * code_to_execute[reg2])
             },// Mult opcode
             99 => Err(IntcodeParseError::Halt),// Halt
             _ => Err(IntcodeParseError::InvalidOpcode),// Err
@@ -45,12 +77,13 @@ fn process_input(input: &str) -> String {
             Err(IntcodeParseError::InvalidOpcode) => panic!("Invalid opcode"),
         };
 
-        let out_reg: usize = *(position.next().expect("Invalid index")) as usize;
+        let out_reg: usize = slice_to_work_on[3] as usize;
         code_to_execute[out_reg] = value;
+        cursor += 4;
     }
 
     // creates a String for the output, separated by commas
-    let result = input_vector.into_iter()
+    let result = code_to_execute.into_iter()
         .map(|int| int.to_string())
         .collect::<Vec<String>>()
         .join(",");
